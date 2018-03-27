ea96b1e0ad0165932aa4dce7409368d74e4952bd
xingzo <kingsleynyaosi@gmail.com>
2018-03-27 10:27:27 -0700

    working initial react SPA

---
+++ src/About.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './styles/About.css';
+
+class About extends Component {
+  // constructor(props){
+    // super();
+    // this.state = {};
+  // }
+
+  // componentWillMount(){}
+  // componentDidMount(){}
+  // componentWillUnmount(){}
+
+  // componentWillReceiveProps(){}
+  // shouldComponentUpdate(){}
+  // componentWillUpdate(){}
+  // componentDidUpdate(){}
+
+  render() {
+    return (
+      <div>heyyy</div>
+    );
+  }
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 import './styles/App.css';
 
+import {
+    Route,
+    Link,
+    Switch
+} from 'react-router-dom'
+
+import About from './About';
+
 class App extends Component {
   render() {
     return (
-        <div>
-            App Component working!
-        </div>
+      <header>
+          <h1>Learn Routing</h1>
+          <nav>
+              {/* Create our nav bar links using the Link element from react router */}
+              <ul>
+                  <li><Link to="/about">About</Link></li>
+              </ul>
+          </nav>
+
+            {/* Create the routes. This will not appear on the page. */}
+          <div className="main">
+              <Switch>
+                  <Route path="/about" component={ About } />
+              </Switch>
+          </div>
+      </header>
     );
   }
 }
